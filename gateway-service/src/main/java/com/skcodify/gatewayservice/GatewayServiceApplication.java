bc1e06be25865e499864516ff28f51a2868b5c92
Shekhar Kumar <shekharkumargupta@gmail.com>
2024-06-18 14:49:35 +0530

    Added keycloak to docker compose

--- gateway-service/src/main/java/com/skcodify/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/skcodify/gatewayservice/GatewayServiceApplication.java
@@ -1,11 +1,13 @@
 package com.skcodify.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@AutoConfiguration
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
--- gateway-service/src/test/java/com/skcodify/gatewayservice/GatewayServiceApplicationTests.java
+++ gateway-service/src/test/java/com/skcodify/gatewayservice/GatewayServiceApplicationTests.java
@@ -1,12 +1,11 @@
 package com.skcodify.gatewayservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GatewayServiceApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
